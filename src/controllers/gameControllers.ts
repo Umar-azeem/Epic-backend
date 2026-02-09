3f583c76ebbaf1ad59fac6e00641db20efea4477
Umar-azeem <umara4436@gmail.com>
2026-02-09 17:35:18 +0500

    fix type error

--- src/controllers/gameControllers.ts
+++ src/controllers/gameControllers.ts
@@ -1,6 +1,7 @@
 import Game from "../models/Game.js";
+import { Request, Response } from "express";
 
-export const createGame = async (req, res) => {
+export const createGame = async (req:Request, res: Response) => {
   try {
     if (!req.body.title) {
       return res.status(400).json({ message: "Title is required" });
@@ -8,20 +9,28 @@ export const createGame = async (req, res) => {
     const game = await Game.create(req.body);
     res.status(201).json(game);
   } catch (error) {
-    res.status(401).json({ message: error.message });
+  if (error instanceof Error) {
+    res.status(400).json({ message: error.message });
+  } else {
+    res.status(400).json({ message: "Unknown error occurred" });
   }
+}
 };
 
-export const getGames = async (req, res) => {
+export const getGames = async (req: Request, res: Response) => {
   try {
     const games = await Game.find();
     res.json(games);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+  if (error instanceof Error) {
+    res.status(400).json({ message: error.message });
+  } else {
+    res.status(400).json({ message: "Unknown error occurred" });
   }
+}
 };
 
-export const getGameById = async (req, res) => {
+export const getGameById = async (req: Request, res: Response) => {
   try {
     const game = await Game.findById(req.params.id);
 
@@ -31,12 +40,16 @@ export const getGameById = async (req, res) => {
 
     res.status(200).json(game);
   } catch (error) {
+  if (error instanceof Error) {
     res.status(400).json({ message: error.message });
+  } else {
+    res.status(400).json({ message: "Unknown error occurred" });
   }
+}
 };
 
 /* ================= UPDATE ================= */
-export const updateGame = async (req, res) => {
+export const updateGame = async (req: Request, res: Response) => {
   try {
     const game = await Game.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
@@ -49,12 +62,16 @@ export const updateGame = async (req, res) => {
 
     res.status(200).json(game);
   } catch (error) {
+  if (error instanceof Error) {
     res.status(400).json({ message: error.message });
+  } else {
+    res.status(400).json({ message: "Unknown error occurred" });
   }
+}
 };
 
 /* ================= DELETE ================= */
-export const deleteGame = async (req, res) => {
+export const deleteGame = async (req: Request, res: Response) => {
   console.log("DELETE CONTROLLER HIT", req.params.id);
 
   try {
@@ -66,6 +83,10 @@ export const deleteGame = async (req, res) => {
 
     res.status(200).json({ message: "Game deleted successfully" });
   } catch (error) {
+  if (error instanceof Error) {
     res.status(400).json({ message: error.message });
+  } else {
+    res.status(400).json({ message: "Unknown error occurred" });
   }
+}
 };
