5082386d1560cf3f9509092cec102e084d02e154
Umar-azeem <umara4436@gmail.com>
2026-04-10 20:33:46 +0500

    add auth routes

--- server.js
+++ server.js
@@ -55,6 +55,6 @@ mongoose
     process.exit(1);
   });
 
- const PORT = process.env.PORT || 5000;
- app.listen(PORT, () => console.log(`Server running → http://localhost:${PORT}`));
+//  const PORT = process.env.PORT || 5000;
+//  app.listen(PORT, () => console.log(`Server running → http://localhost:${PORT}`));
 export default app;
